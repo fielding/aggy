38cf451453254f600c14ec97e8a4c5d1df773280
Fielding Johnston <fielding@justfielding.com>
2018-12-24 07:31:37 -0600

    Rewrite/Refactor with consistent promise based asynchronicity in mind

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-// TODO: fix the sync/async inconsistencies and rewrite
-// exclusively using promises
-const {
-  lstatSync,
-  mkdir,
-  readdirSync,
-  existsSync,
-  readFile,
-  writeFile,
-} = require('fs');
-const { join } = require('path');
+const { mkdir, readdir, readFile, stat, writeFile } = require('fs').promises;
+const { join, normalize, dirname, resolve  } = require('path');
 const { hostname } = require('os');
-const { promisify } = require('util');
 const untildify = require('untildify');
 const git2json = require('@fabien0102/git2json');
 
-const readFileAsync = promisify(readFile);
-const writeFileAsync = promisify(writeFile);
-
 // CONSTANTS
+const repoPaths = ['~/etc', '~/src/hack', '/Volumes/stuff/cloud/Dropbox/work'];
+const user = 'fielding@justfielding.com';
 const DB = '~/.local/share/quantified-self/commits.json';
+const dbPath = normalize(untildify(DB));
 
-const isDirectory = path => lstatSync(path).isDirectory();
-const isRepository = path => existsSync(join(path, '.git'));
+const isErrorNotFound = err => err.code === 'ENOENT';
 const isUser = (commit, userEmail) => commit.author.email === userEmail;
 
 const uuidFromSha1 = hash =>
@@ -32,96 +21,99 @@ const uuidFromSha1 = hash =>
   hash.substring(18, 20) +
   '-' + hash.substring(20, 32);
 
-const expandRepoPaths = paths =>
-  paths.map(p => untildify(p)).reduce((acc, p) => {
-    let res;
-    if (isRepository(p)) {
-      res = [p];
-    } else {
-      try {
-        res = readdirSync(p)
-          .map(name => join(p, name))
-          .filter(isDirectory && isRepository);
-      } catch (err) {
-        if (err.code === 'ENOENT') {
-          console.error(`Skipping "${p}", because the directory was not found! `);
-          res = [];
-        } else {
-          throw err;
-        }
-      }
+// TODO: I wish I was good at naming functions like this one.
+const normalizePaths = paths =>
+  paths
+    .map(untildify)
+    .map(normalize);
 
-    }
-
-    return [...acc, ...res];
-  }, []);
-
-const repoHistories = (repositoryPaths, userEmail) =>
-  expandRepoPaths(repositoryPaths).map(path =>
-    git2json
-      .run({ path })
-      .then(json => json
-        .filter(commit => isUser(commit, userEmail))
-        .map(commit =>
-          Object.assign(
-            {
-              timestamp: commit.author.timestamp,
-              dataType: 'activity-hack',
-              dataSource: `git@${hostname}`,
-              uuid: uuidFromSha1(commit.hash),
-            },
-            commit
-          )
-        )
-      )
-      .catch(err => {
-        console.error(`Can't get git log for ${path} :: ${err})`);
-        return [];
-      })
-  );
-
-// TODO: is there any reason to factor this out further? Thinking that if I do
-// then a change in wording on a few things is in order.
-
-
-const getPrevious = db =>
-  readFileAsync(db, { encoding: 'utf8' })
-    .then(contents => JSON.parse(contents))
-    .catch(() => {
-      console.error(`No previous history found at ${db}`);
-      // TODO: fix this and other sync/async inconsistencies
-      mkdir(db.substr(0, db.lastIndexOf('/')), { recursive: true }, err => {
-        if (err) throw err;
-      });
-      return [];
-    });
+const flatten = (arr, depth = 1) =>
+  arr.reduce((a, v) => a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1) : v), []);
 
 const duplicate = (commit, index, self) =>
   index === self.findIndex(c => (
-    c.uuid === commit.uuid && c.timestamp === commit.timestamp
+    c.dataSource === commit.dataSource && c.uuid === commit.uuid
   ));
+
+const recurse = path =>
+  readdir(path)
+    .then(list =>
+      Promise.all(list.map(file => {
+        const fullpath = resolve(path, file);
+        return stat(fullpath)
+          .then(stats => {
+            if (stats.isDirectory()) {
+              return isRepository(fullpath);
+            }
+        });
+      })))
+    .then(results => results.filter(Boolean))
+    .then(flatten);
+
+// TODO: test to make sure that nested repositories are detected
+const isRepository = path =>
+  stat(join(path, '.git'))
+    .then(gPath => {
+      if (gPath.isDirectory()) {
+        return path;
+      }
+      return recurse(path);
+    })
+    .catch(err => {
+      if (isErrorNotFound(err)) {
+        return recurse(path);
+      }
+      throw err;
+    });
+
+// The word history or histories refers to the commit histories
+// and the aggregate commit history we are creating.
+
+const findRepositories = paths =>
+  Promise.all(normalizePaths(paths).map(path => isRepository(path)))
+    .then(flatten);
+
+const getRepositoryHistories = (repositoryPaths, userEmail) =>
+  findRepositories(repositoryPaths)
+    .then(repos => Promise.all(repos.map(path =>
+      git2json
+        .run({ path })
+        .then(json => json
+          .filter(commit => isUser(commit, userEmail))
+          .map(commit =>
+            Object.assign(
+              {
+                timestamp: commit.author.timestamp,
+                dataType: 'activity-hack',
+                dataSource: `git@${hostname}:${path}`,
+                uuid: uuidFromSha1(commit.hash),
+
+              },
+              commit
+            )))
+        .catch(err => {
+          console.error(`Can't get git log for ${path} :: ${err})`);
+          return [];
+        }))));
+
+const getPreviousHistory = db =>
+  readFile(db, { encoding: 'utf8' })
+    .then(contents => JSON.parse(contents))
+    .catch(() => {
+      console.error(`No previous history found at ${db}.`);
+      return [];
+    });
 
 const getUpdatedHistory = (repositoryPaths, userEmail) =>
   Promise.all([
-    getPrevious(untildify(DB)),
-    ...repoHistories(repositoryPaths, userEmail),
+    getPreviousHistory(dbPath),
+    getRepositoryHistories(repositoryPaths, userEmail),
   ])
+    .then(flatten)
     .then(history => [].concat(...history).filter(duplicate));
 
-const writeUpdatedHistory = ( repositoryPaths, userEmail ) =>
-  getUpdatedHistory(repositoryPaths, userEmail)
-    .then(history => writeFileAsync(untildify(DB), JSON.stringify(history,
-      null, '  ')));
-//
-//
-//
-//
-//
-//
-//
-//
-//
-const repos = ['~/etc', '~/src/hack', '~/cloud/Dropbox/work'];
-const user = 'fielding@justfielding.com';
+const writeToDB = history =>
+  mkdir(dirname(dbPath), { recursive: true })
+    .then(writeFile(dbPath, JSON.stringify(history, null, '  ')));
 
 writeUpdatedHistory( repos, user );
